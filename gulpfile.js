e7451a407cd7c76ff5807c4a80b92b63631d0140
Kimberly Grey <kimandthemachine@gmail.com>
2016-08-19 20:34:09 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,113 @@
+'use strict';
+
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['watch']);
+gulp.task('lint', ['sass:lint', 'js:lint'])
+gulp.task('production', ['sass', 'js', 'images', 'fonts']);
+gulp.task('force', ['production']);
+
+gulp.task('watch', () => {
+	gulp.watch('./src/scss/{,*/}*.scss', ['sass']);
+	gulp.watch('./src/js/scripts/{,*/}*.js', ['js']);
+	gulp.watch('./src/js/+(preload|vendor)/{,*/}*.js', ['js:vendor']);
+	gulp.watch('./src/images/{,*/}*', ['images']);
+	gulp.watch('./src/type/{,*/}*', ['fonts']);
+});
+
 /*
-	Gulp is a system that automates various tasks, such as linting, 
-	compiling and minifying files, and obfuscating javascript.
+ * Sass compilation
+ */
 
-	FIRST YOU'LL NEED THIS:
-	Node.js (http://nodejs.org/)
-
-	Navigate to the project dependency using the Node.js command prompt,
-	then run this to install the project dependencies:
-	  $ npm install
-
-	If some src files already exist you'll want to compile them all:
-	  $ gulp force
-
-	And if you're making further changes run this to watch for changes
-	and compile automatically:
-	  $ gulp
-	
-	Sorted.
-*/
-
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var newer = require("gulp-newer");
-var imagemin = require("gulp-imagemin");
-var plumber = require("gulp-plumber");
-var sourcemaps = require("gulp-sourcemaps");
-var merge = require("merge-stream");
-
-gulp.task("default", function() {
-	gulp.watch("./src/scss/{,*/}*.scss", ["stylesheets"]);
-	gulp.watch("./src/js//{,*/}*.js", ["javascript"]);
-	gulp.watch("./src/type/{,*/}*.js", ["type"]);
-});
-
-gulp.task("force", ["stylesheets", "javascript", "images", "type"]);
-
-gulp.task("stylesheets", function() {
-	gulp.src("./src/scss/stylesheet.scss")
+gulp.task('sass', () => {
+	const sass = require('gulp-sass');
+	const autoprefixer = require('gulp-autoprefixer');
+	gulp.src('./src/scss/*.scss')
 	.pipe(plumber())
-	.pipe(sourcemaps.init())
-	.pipe(sass(
-		{
-			errLogToConsole: true,
-			outputStyle: "compressed"
-		}
-	))
-	.pipe(autoprefixer(
-		{
-			browsers: ['last 2 version', 'ie 8', 'ie 9', 'ie 10'],
-			cascade: true
-		}
-	))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("./dst/css"))
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}))
+	.pipe(autoprefixer({
+		browsers: ['last 2 version', 'ie 9', 'ie 10'],
+		cascade: true
+	}))
+	.pipe(gulp.dest('./dst/css'));
 });
 
-gulp.task("javascript", function() {
-	var folders = ["preload", "vendor", "scripts"];
-	var tasks = folders.map(function(folder) {
-		return gulp.src("./src/js/" + folder + "/**/*.js", {
-			base: "./src/js/" + folder
+gulp.task('sass:lint', () => {
+	const sassLint = require('gulp-sass-lint');
+	// The sass-lint npm module is broken at the moment, so keeping this disabled.
+	// https://github.com/sasstools/sass-lint/issues/389
+	// return gulp.src('./src/scss/{,*/}*.scss')
+	// .pipe(plumber())
+	// .pipe(sassLint({
+	// 	'config': '.sass-lint.yml'
+	// }))
+	// .pipe(sassLint.format())
+	// .pipe(sassLint.failOnError());
+});
+
+/**
+ * JavaScript compilation
+ */
+
+gulp.task('js', () => {
+	const babel = require('gulp-babel');
+	const concat = require('gulp-concat');
+	const merge = require('merge-stream');
+	var folders = ['scripts'];
+	var tasks = folders.map((folder) => {
+		return gulp.src(`./src/js/${folder}/{,*/}*.js`, {
+			base: `./src/js/${folder}`
 		})
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
-		.pipe(concat(folder + ".js"))
-		.pipe(uglify())
-		.pipe(sourcemaps.init())
-		.pipe(gulp.dest("./dst/js"))
+		.pipe(babel(
+			{
+				presets: ['es2015']
+			}
+		))
+		.pipe(concat(`${folder}.js`))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dst/js'))
 	});
 	merge(tasks);
 });
 
-gulp.task("images", function() {
-	gulp.src("./src/images/{,*/}*")
-	.pipe(newer("./dst/images"))
+gulp.task('js:vendor', () => {
+	const concat = require('gulp-concat');
+	const merge = require('merge-stream');
+	const folders = ['preload', 'vendor'];
+	const tasks = folders.map((folder) => {
+		return gulp.src(`./src/js/${folder}/{,*/}*.js`, {
+			base: `./src/js/${folder}`
+		})
+		.pipe(plumber())
+		.pipe(concat(`${folder}.js`))
+		.pipe(gulp.dest('./dst/js'))
+	});
+	merge(tasks);
+});
+
+gulp.task('js:lint', () => {
+	const jshint = require('gulp-jshint');
+	return gulp.src(['./src/js/{,*/}*.js', '!./src/js/vendor/{,*/}*.js'])
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
+
+/**
+ * Image optimisation
+ */
+
+gulp.task('images', () => {
+	const newer = require('gulp-newer');
+	const imagemin = require('gulp-imagemin');
+	gulp.src('./src/images/{,*/}*')
+	.pipe(plumber())
+	.pipe(newer('./dst/images'))
 	.pipe(imagemin(
 		{
 			optimizationLevel: 5, // png
@@ -85,10 +116,14 @@ gulp.task("images", function() {
 			multipass: true // svg
 		}
 	))
-	.pipe(gulp.dest("./dst/images"))
+	.pipe(gulp.dest('./dst/images'));
 });
 
-gulp.task("type", function() {
-	gulp.src("./src/type/{,*/}*")
-	.pipe(gulp.dest("./dst/type"))
+/**
+ * Typography teleportation
+ */
+
+gulp.task('fonts', () => {
+	gulp.src('./src/type/{,*/}*')
+	.pipe(gulp.dest('./dst/type'));
 });
