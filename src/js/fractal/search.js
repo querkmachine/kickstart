0f7b2df2b33058a1e277363789690f07a95f1ff3
Kimberly Grey <kimandthemachine@gmail.com>
2018-07-15 20:02:32 +0100

    A very simple search

---
+++ src/js/fractal/search.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+    'use strict';
+    var $searchInput = jQuery('#kimsFractalSearch');
+    var $componentList = jQuery('.Tree-entityLink');
+    //console.log($searchInput, $componentList);
+    jQuery(document).on('keyup', $searchInput, function() {
+        var searchQuery = $searchInput.val();
+        jQuery('.Tree-collection.is-closed > .Tree-collectionLabel').trigger('click');
+        if(searchQuery === '') {
+            $componentList.show();
+        }
+        else {
+            $componentList.each(function(i, item) { 
+                var $item = jQuery(item);
+                var trimmedText = $item.text().trim().toLowerCase();
+                //console.log(trimmedText);
+                if(trimmedText.indexOf(searchQuery.toLowerCase()) === -1) {
+                    $item.hide();
+                }
+                else {
+                    $item.show();
+                }
+            });
+        }
+    });
+});
