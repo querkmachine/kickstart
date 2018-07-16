7946d756a63da08182bc35f514269d1003acdcea
Kimberly Grey <kimandthemachine@gmail.com>
2018-07-16 20:45:35 +0100

    Make it so that emptying the input closes accordions

--- src/js/fractal/search.js
+++ src/js/fractal/search.js
@@ -5,11 +5,12 @@ jQuery(document).ready(function() {
     //console.log($searchInput, $componentList);
     jQuery(document).on('keyup', $searchInput, function() {
         var searchQuery = $searchInput.val();
-        jQuery('.Tree-collection.is-closed > .Tree-collectionLabel').trigger('click');
         if(searchQuery === '') {
+            jQuery('.Tree-collection:not(.is-closed) > .Tree-collectionLabel').trigger('click');
             $componentList.show();
         }
         else {
+            jQuery('.Tree-collection.is-closed > .Tree-collectionLabel').trigger('click');
             $componentList.each(function(i, item) { 
                 var $item = jQuery(item);
                 var trimmedText = $item.text().trim().toLowerCase();
